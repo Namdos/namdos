32d6c07a4c5c6f4e32ce75a4e3ec6376b4b3a065
NUMERO-UNO\Azubi1 <johannes@ab716.de>
2018-08-21 13:46:15 +0200

    initial commit

---
+++ public/home/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Johannes Marius Brüggemann
+ * Date: 07.06.2018
+ * Time: 14:52
+ */
+echo '<h1>Hello World!</h1>';
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Namdos</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+            <div class="content" style="z-index: 9999; position: absolute; top: 50%; transform: translateY(-50%);">
+                <h1>Namdos</h1>
+                <div class="subline">
+                    <h3>Under Construction</h3>
+                </div>
+                <div class="block1"></div>
+                <!--
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+                -->
+            </div>
+        </div>
+    </body>
+</html>
